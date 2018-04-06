3e4ae617d7c3e32ea1069d9370061a91a86f442e
Lucas Lombardo <Lucasl@bu.edu>
2018-04-06 13:50:23 -0400

    add baseline styling and buttons

---
+++ scripts/casio.js
@@ -0,0 +1,12 @@
+window.onload = function(){
+
+};
+
+var value 		= "",
+	display 	= "",
+	state 		= "blank"; //3 states, "blank", "operating", and "numPartial"
+
+function numPush(){
+	//if numpartial == num, add to num
+	
+}
